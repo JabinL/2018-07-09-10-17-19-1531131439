f1976ff0e3b3b363f38d22843b19347b19930447
JABIN LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <LIANGJA6@OOCL.COM>
2020-07-22 17:42:11 +0800

    OO Step by Step[Jabin] feature: finished question 1-6

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,22 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+    private  int Klass;
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.Klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return "I am a Student."+" I am at Class "+this.Klass+".";
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
 }
--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,23 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+
+    private int Klass;
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.Klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Student. I am at Class "+this.Klass+".";
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
 }
