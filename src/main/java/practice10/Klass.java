1d957503bb7612e0fdfc7a6dc470127fbb77b3ad
Jabin <2099008974@qq.com>
2020-07-22 23:28:09 +0800

    OO Step by Step[Jabin] feature:finished practice 10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,58 @@
 package practice10;
 
+
+
 public class Klass {
+
+    private int number;
+    private Student leader;
+    private Student member;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Student getMember() {
+        return member;
+    }
+
+    public void setMember(Student member) {
+        this.member = member;
+    }
+
+    public String getDisplayName(){
+        return "Class "+this.number;
+    }
+    public void assignLeader(Student student){
+        if(this.member!=null){
+            if(this.member.equals(student))
+                this.leader = student;
+        }else
+            System.out.print("It is not one of us.\n");
+
+    }
+
+    public void appendMember(Student student){
+        this.member = student;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj != null)
+            return this.number ==  ((Klass) obj).getNumber();
+        return false;
+    }
+
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,70 @@
 package practice10;
 
-public class Teacher {
+
+
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(LinkedList<Klass> classes) {
+        this.classes = classes;
+    }
+
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+
+    @Override
+    public String introduce() {
+        if(this.classes != null) {
+            if (this.classes.size() > 0) {
+                String str = "" + this.classes.get(0).getNumber();
+                for (int i = 1; i < this.classes.size(); i++) {
+                    Klass klass = this.classes.get(i);
+
+                        str += ", " + klass.getNumber();
+
+
+                }
+                return super.introduce() + " I am a Teacher. I teach Class " + str + ".";
+            }
+        }
+         return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
+    public String introduceWith(Student student) {
+            if(this.classes!=null){
+                for(int i=0; i < this.classes.size(); i++){
+                    if (this.classes.get(i).equals(student.getKlass()))
+                         return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+             }
+            }
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
+
+    public boolean isTeaching(Student student){
+        if(student==null)
+            return false;
+        if(this.classes.size()>0){
+            for(int i=0; i< this.classes.size(); i++){
+                if(this.classes.get(i).equals(student.getKlass())){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
