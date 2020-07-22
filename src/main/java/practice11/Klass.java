5262efcd4c284ffb5ece7cee9fc7b138d9de63e3
Jabin <2099008974@qq.com>
2020-07-23 01:18:41 +0800

    OO Step by Step[Jabin] feature:finished practice 11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,81 @@
 package practice11;
 
+
 public class Klass {
+
+    private int number;
+    private Student leader;
+    private Student member;
+    private Teacher teacher; //观察者
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Student getMember() {
+        return member;
+    }
+
+    public void setMember(Student member) {
+        this.member = member;
+    }
+
+    public String getDisplayName(){
+        return "Class "+this.number;
+    }
+    public void assignLeader(Student student){
+        if(student == null)
+            return;
+        if(this.member!=null){
+            if(this.member.equals(student))
+            {
+                this.leader = student;
+                if(this.teacher!=null)
+                    System.out.print(this.teacher.update()+" I know "+student.getName()+" become Leader of Class "+this.getNumber()+".\n");
+            }
+        }else
+            System.out.print("It is not one of us.\n");
+
+
+
+    }
+
+    public void appendMember(Student student){
+        if(student == null)
+            return;
+        this.member = student;
+        if(this.getLeader()==null)
+            if(this.teacher != null)
+                System.out.print(this.teacher.update()+" I know "+student.getName()+" has joined Class "+this.getNumber()+".\n");
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj != null)
+            return this.number ==  ((Klass) obj).getNumber();
+        return false;
+    }
+
+
 }
